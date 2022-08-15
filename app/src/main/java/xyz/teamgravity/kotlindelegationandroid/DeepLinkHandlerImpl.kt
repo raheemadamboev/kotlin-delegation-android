e91365bd0e81d2a0ae4f080d64ec7e25a30b9f1c
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-15 16:51:39 +0500

    add implementation for deep link handler

---
+++ app/src/main/java/xyz/teamgravity/kotlindelegationandroid/DeepLinkHandlerImpl.kt
@@ -0,0 +1,11 @@
+package xyz.teamgravity.kotlindelegationandroid
+
+import android.app.Activity
+import android.content.Intent
+
+class DeepLinkHandlerImpl : DeepLinkHandler {
+
+    override fun handleDeepLink(activity: Activity, intent: Intent?) {
+        // do something
+    }
+}
